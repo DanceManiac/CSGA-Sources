4933fbf53ea37c6b2ca80b734b131f2600f7597a
Shtrecker <77389805+Shtrecker@users.noreply.github.com>
2022-08-09 18:29:03 +0300

    Анимации двухстволкам

--- xray/xrGame/weaponBM16.cpp
+++ xray/xrGame/weaponBM16.cpp
@@ -8,15 +8,28 @@ CWeaponBM16::~CWeaponBM16()
 void CWeaponBM16::Load	(LPCSTR section)
 {
 	inherited::Load		(section);
-	m_sounds.LoadSound	(section, "snd_reload_1", "sndReload1", m_eSoundShot);
+	m_sounds.LoadSound	(section, "snd_reload_1", "sndReload1", m_eSoundReload);
+	m_sounds.LoadSound	(section, "snd_reload_jammed", "sndReloadJammed", m_eSoundReload);
+	m_sounds.LoadSound	(section, "snd_reload_jammed_last", "sndReloadJammedLast", m_eSoundReload);
 }
 
 void CWeaponBM16::PlayReloadSound()
 {
-	if(m_magazine.size()==1)	
-		PlaySound	("sndReload1",get_LastFP());
-	else						
-		PlaySound	("sndReload",get_LastFP());
+	switch( m_magazine.size() )
+	{
+	case 1:
+		if(IsMisfire())
+			PlaySound	("sndReloadJammed",get_LastFP());
+		else
+			PlaySound	("sndReload1",get_LastFP());
+		break;
+	case 2:
+		if(IsMisfire())
+			PlaySound	("sndReloadJammedLast",get_LastFP());
+		else
+			PlaySound	("sndReload",get_LastFP());
+		break;
+	}
 }
 
 void CWeaponBM16::PlayAnimShoot()
@@ -24,10 +37,16 @@ void CWeaponBM16::PlayAnimShoot()
 	switch( m_magazine.size() )
 	{
 	case 1:
-		PlayHUDMotion("anm_shot_1",FALSE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_shoot_jammed_1",FALSE,this,GetState());
+		else
+			PlayHUDMotion("anm_shoot_1",FALSE,this,GetState());
 		break;
 	case 2:
-		PlayHUDMotion("anm_shot_2",FALSE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_shoot_jammed_2",FALSE,this,GetState());
+		else
+			PlayHUDMotion("anm_shoot_2",FALSE,this,GetState());
 		break;
 	}
 }
@@ -37,13 +56,22 @@ void CWeaponBM16::PlayAnimShow()
 	switch( m_magazine.size() )
 	{
 	case 0:
-		PlayHUDMotion("anm_show_0",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_show_jammed_0",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_show_0",TRUE,this,GetState());
 		break;
 	case 1:
-		PlayHUDMotion("anm_show_1",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_show_jammed_1",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_show_1",TRUE,this,GetState());
 		break;
 	case 2:
-		PlayHUDMotion("anm_show_2",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_show_jammed_2",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_show_2",TRUE,this,GetState());
 		break;
 	}
 }
@@ -53,18 +81,27 @@ void CWeaponBM16::PlayAnimHide()
 	switch( m_magazine.size() )
 	{
 	case 0:
-		PlayHUDMotion("anm_hide_0",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_hide_jammed_0",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_hide_0",TRUE,this,GetState());
 		break;
 	case 1:
-		PlayHUDMotion("anm_hide_1",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_hide_jammed_1",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_hide_1",TRUE,this,GetState());
 		break;
 	case 2:
-		PlayHUDMotion("anm_hide_2",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_hide_jammed_2",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_hide_2",TRUE,this,GetState());
 		break;
 	}
 }
 
-void CWeaponBM16::PlayAnimBore()
+void CWeaponBM16::PlayAnimBore()//в ганс двухстволках не юзаются анимы скуки
 {
 	switch( m_magazine.size() )
 	{
@@ -86,9 +123,15 @@ void CWeaponBM16::PlayAnimReload()
 
 	VERIFY(GetState()==eReload);
 	if(m_magazine.size()==1 || !b_both)
-		PlayHUDMotion("anm_reload_1",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_reload_jammed_1",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_reload_1",TRUE,this,GetState());
 	else
-		PlayHUDMotion("anm_reload_2",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_reload_jammed_2",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_reload_2",TRUE,this,GetState());
 }
 
 void  CWeaponBM16::PlayAnimIdleMoving()
@@ -96,13 +139,22 @@ void  CWeaponBM16::PlayAnimIdleMoving()
 	switch( m_magazine.size() )
 	{
 	case 0:
-		PlayHUDMotion("anm_idle_moving_0",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_idle_moving_jammed_0",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_idle_moving_0",TRUE,this,GetState());
 		break;
 	case 1:
-		PlayHUDMotion("anm_idle_moving_1",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_idle_moving_jammed_1",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_idle_moving_1",TRUE,this,GetState());
 		break;
 	case 2:
-		PlayHUDMotion("anm_idle_moving_2",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_idle_moving_jammed_2",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_idle_moving_2",TRUE,this,GetState());
 		break;
 	}
 }
@@ -112,13 +164,22 @@ void  CWeaponBM16::PlayAnimIdleSprint()
 	switch( m_magazine.size() )
 	{
 	case 0:
-		PlayHUDMotion("anm_idle_sprint_0",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_idle_sprint_jammed_0",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_idle_sprint_0",TRUE,this,GetState());
 		break;
 	case 1:
-		PlayHUDMotion("anm_idle_sprint_1",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_idle_sprint_jammed_1",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_idle_sprint_1",TRUE,this,GetState());
 		break;
 	case 2:
-		PlayHUDMotion("anm_idle_sprint_2",TRUE,this,GetState());
+		if(IsMisfire())
+			PlayHUDMotion("anm_idle_sprint_jammed_2",TRUE,this,GetState());
+		else
+			PlayHUDMotion("anm_idle_sprint_2",TRUE,this,GetState());
 		break;
 	}
 }
@@ -131,27 +192,53 @@ void CWeaponBM16::PlayAnimIdle()
 	{
 		switch (m_magazine.size())
 		{
-		case 0:{
-			PlayHUDMotion("anm_idle_aim_0", TRUE, NULL, GetState());
+		case 0:
+		{
+			if(IsMisfire())
+				PlayHUDMotion("anm_idle_aim_jammed_0", TRUE, NULL, GetState());
+			else
+				PlayHUDMotion("anm_idle_aim_0", TRUE, NULL, GetState());
 		}break;
-		case 1:{
-			PlayHUDMotion("anm_idle_aim_1", TRUE, NULL, GetState());
+		case 1:
+		{
+			if(IsMisfire())
+				PlayHUDMotion("anm_idle_aim_jammed_1", TRUE, NULL, GetState());
+			else
+				PlayHUDMotion("anm_idle_aim_1", TRUE, NULL, GetState());
 		}break;
-		case 2:{
-			PlayHUDMotion("anm_idle_aim_2", TRUE, NULL, GetState());
+		case 2:
+		{
+			if(IsMisfire())
+				PlayHUDMotion("anm_idle_aim_jammed_2", TRUE, NULL, GetState());
+			else
+				PlayHUDMotion("anm_idle_aim_2", TRUE, NULL, GetState());
 		}break;
 		};
-	}else{
+	}
+	else
+	{
 		switch (m_magazine.size())
 		{
-		case 0:{
-			PlayHUDMotion("anm_idle_0", TRUE, NULL, GetState());
+		case 0:
+		{
+			if(IsMisfire())
+				PlayHUDMotion("anm_idle_jammed_0", TRUE, NULL, GetState());
+			else
+				PlayHUDMotion("anm_idle_0", TRUE, NULL, GetState());
 		}break;
-		case 1:{
-			PlayHUDMotion("anm_idle_1", TRUE, NULL, GetState());
+		case 1:
+		{
+			if(IsMisfire())
+				PlayHUDMotion("anm_idle_jammed_1", TRUE, NULL, GetState());
+			else
+				PlayHUDMotion("anm_idle_1", TRUE, NULL, GetState());
 		}break;
-		case 2:{
-			PlayHUDMotion("anm_idle_2", TRUE, NULL, GetState());
+		case 2:
+		{
+			if(IsMisfire())
+				PlayHUDMotion("anm_idle_jammed_2", TRUE, NULL, GetState());
+			else
+				PlayHUDMotion("anm_idle_2", TRUE, NULL, GetState());
 		}break;
 		};
 	}
