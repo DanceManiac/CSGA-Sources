83d4591a1288806924086926bf170c2ef45137b1
Shtrecker <77389805+Shtrecker@users.noreply.github.com>
2022-10-16 12:53:53 +0300

    scope_zoom_factor как в гансе

--- xray/xrGame/Actor.cpp
+++ xray/xrGame/Actor.cpp
@@ -832,24 +832,26 @@ void CActor::g_Physics			(Fvector& _accel, float jump, float dt)
 	}
 }
 float g_fov = 67.5f;
+float g_scope_fov = 75.0f;
 
 float CActor::currentFOV()
 {
-	if (!psHUD_Flags.is(HUD_WEAPON|HUD_WEAPON_RT|HUD_WEAPON_RT2))
-		return g_fov;
+    if (!psHUD_Flags.is(HUD_WEAPON | HUD_WEAPON_RT | HUD_WEAPON_RT2))
+        return g_fov;
 
-	CWeapon* pWeapon = smart_cast<CWeapon*>(inventory().ActiveItem());	
+	if (eacFirstEye == cam_active)
+	{
+		CWeapon* pWeapon = smart_cast<CWeapon*>(inventory().ActiveItem());
+		if (pWeapon && pWeapon->IsZoomed())
+		{
+			if (!pWeapon->ZoomTexture())
+				return atan(tan(g_fov * (0.5 * PI / 180)) / pWeapon->GetZoomFactor()) / (0.5 * PI / 180); //Alun: For iron sights, we use camera fov
 
-	if (eacFirstEye == cam_active && pWeapon &&
-		pWeapon->IsZoomed() && 
-		( !pWeapon->ZoomTexture() || (!pWeapon->IsRotatingToZoom() && pWeapon->ZoomTexture()) )
-		 )
-	{
-		return pWeapon->GetZoomFactor() * (0.75f);
-	}else
-	{
-		return g_fov;
+			if (!pWeapon->IsRotatingToZoom())
+				return atan(tan(g_scope_fov * (0.5 * PI / 180)) / pWeapon->GetZoomFactor()) / (0.5 * PI / 180); //Alun: This assumes scope has a fake FOV so that no matter camera FOV the scope FOV is exactly the same
+		}
 	}
+	return g_fov;
 }
 
 void CActor::UpdateCL	()
