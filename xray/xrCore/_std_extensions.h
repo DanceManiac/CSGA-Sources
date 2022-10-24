a3d3d07529bf7a793a5d02ed5e868d5a31662cae
Hrusteckiy <kostia4554t@yandex.ru>
2022-10-24 18:25:16 +0300

    xr_sprintf

--- xray/xrCore/_std_extensions.h
+++ xray/xrCore/_std_extensions.h
@@ -102,14 +102,14 @@ IC float	_sin	(float x)		{ return sinf(x); }
 IC float	_cos	(float x)		{ return cosf(x); }
 IC BOOL		_valid	(const float x)
 {
-	// check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
+	// check for: Signaling NaN, Quiet NaN, Negative infinity ( â€“INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
 	int			cls			= _fpclass		(double(x));
 	if (cls&(_FPCLASS_SNAN+_FPCLASS_QNAN+_FPCLASS_NINF+_FPCLASS_PINF+_FPCLASS_ND+_FPCLASS_PD))	
 		return	false;	
 
 	/*	*****other cases are*****
 	_FPCLASS_NN Negative normalized non-zero 
-	_FPCLASS_NZ Negative zero ( – 0) 
+	_FPCLASS_NZ Negative zero ( â€“ 0) 
 	_FPCLASS_PZ Positive 0 (+0) 
 	_FPCLASS_PN Positive normalized non-zero 
 	*/
@@ -124,14 +124,14 @@ IC double	_sin	(double x)		{ return sin(x); }
 IC double	_cos	(double x)		{ return cos(x); }
 IC BOOL		_valid	(const double x)
 {
-	// check for: Signaling NaN, Quiet NaN, Negative infinity ( –INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
+	// check for: Signaling NaN, Quiet NaN, Negative infinity ( â€“INF), Positive infinity (+INF), Negative denormalized, Positive denormalized
 	int			cls			= _fpclass		(x);
 	if (cls&(_FPCLASS_SNAN+_FPCLASS_QNAN+_FPCLASS_NINF+_FPCLASS_PINF+_FPCLASS_ND+_FPCLASS_PD))	
 		return false;	
 
 	/*	*****other cases are*****
 	_FPCLASS_NN Negative normalized non-zero 
-	_FPCLASS_NZ Negative zero ( – 0) 
+	_FPCLASS_NZ Negative zero ( â€“ 0) 
 	_FPCLASS_PZ Positive 0 (+0) 
 	_FPCLASS_PN Positive normalized non-zero 
 	*/
@@ -178,6 +178,20 @@ IC u32							xr_strlen				( const char* S )
 IC char*						xr_strlwr				(char* S)
 {	return strlwr(S);				}
 
+inline int __cdecl xr_sprintf	( LPSTR destination, size_t const buffer_size, LPCSTR format_string, ... )
+{
+	va_list args;
+	va_start					( args, format_string);
+	return						vsnprintf_s( destination, buffer_size, buffer_size - 1, format_string, args );
+}
+
+template <int count>
+inline int __cdecl xr_sprintf	( char (&destination)[count], LPCSTR format_string, ... )
+{
+	va_list args;
+	va_start					( args, format_string);
+	return						vsnprintf_s( destination, count, count - 1, format_string, args );
+}
 #ifdef BREAK_AT_STRCMP
 XRCORE_API	int					xr_strcmp				( const char* S1, const char* S2 );
 #else
